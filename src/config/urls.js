c1cde17a81a6692adb643421f07dbc1d28ffde9f
Yohannes mitike <jonmersha@gmail.com>
2026-07-27 14:26:23 +0300

    backend

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,2 +1,2 @@
 export const REMOTE_AUDIT_LOG_URL = ''; // Leave empty if not configured
-export const AUTH_SERVICE_URL = 'https://auth.besheger.com/api'; // Auth service URL
+export const AUTH_SERVICE_URL = 'https://auth.besheger.com'; // Auth service URL
